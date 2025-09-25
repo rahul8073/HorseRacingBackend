577aaf9b04b51e45776acbae87c24b65365adfbd
rahul8073 <rahulkumarsingh1473@gmail.com>
2025-09-25 22:40:26 +0530

    fix issue

--- Routes/Horses.js
+++ Routes/Horses.js
@@ -6,7 +6,7 @@ router.post('/createHorse',admin, HorsesController.CreateHorse);
 router.get('/getAllHorse',admin, HorsesController.GetAllHorses);
 router.get('/getHorseById/:id',admin, HorsesController.GetHorseById);
 router.post('/updateHorse/:id', admin,HorsesController.UpdateHorse);
-router.get('/deleteHorse/:id',admin, HorsesController.deleteHorse);
-router.post('/deleteBulkHorse',admin, HorsesController.deleteHorse);
+router.get('/deleteHorse/:id',admin, HorsesController.DeleteHorse);
+router.post('/deleteBulkHorse',admin, HorsesController.DeleteHorse);
 
 module.exports = router;
