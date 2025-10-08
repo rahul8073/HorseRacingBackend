45fe16dadc6fff9c3de9c12523f3b1237d2125e7
rahul8073 <rahulkumarsingh1473@gmail.com>
2025-10-08 22:00:56 +0530

    fix it

--- controller/HorseBetController.js
+++ controller/HorseBetController.js
@@ -804,9 +804,7 @@ exports.DecideRaceResult = async (req, res) => {
     const raceMode = totalBetAmount < totalWinningAmount ? "High" : "Low";
 
     const historyData = validBets.map(bet => {
-      const isWinner = winningBet
-        ? bet._id.toString() === winningBet._id.toString()
-        : bet.horseId.horseNumber === winningHorse.horseNumber;
+      const isWinner = bet.horseId.horseNumber === winningHorse.horseNumber;
       return {
         userId: bet.userId._id,
         horseId: bet.horseId._id,
@@ -819,6 +817,7 @@ exports.DecideRaceResult = async (req, res) => {
         raceDate: new Date(),
       };
     });
+console.log("winning users:",winningUsers);
 
     if (historyData.length > 0) await BetHistory.insertMany(historyData);
 
