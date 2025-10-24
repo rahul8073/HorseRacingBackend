0bec041f84828f1af255e689b45a0965ad57c47a
rahul8073 <rahulkumarsingh1473@gmail.com>
2025-10-24 22:00:39 +0530

    fix it all

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,11 +1,12 @@
 const express=require('express');
-const { register, login, getProfile, updateProfile, changePassword } = require('../controller/Auth');
+const { register, login, getProfile, updateProfile, changePassword, logout } = require('../controller/Auth');
 const auth = require('../middleware/middleware');
 const admin = require('../middleware/Admin');
 const router=express.Router();
 
 router.post('/register',register)
 router.post('/login',login)
+router.post('/logout',auth,admin,logout)
 router.put("/updateProfile", auth,admin, changePassword);
 router.get("/profile", auth,admin, getProfile);
 router.put("/updateProfile", auth,admin, updateProfile);
